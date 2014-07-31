52a0f8095f6061a3ac373543c9b27cf8cf2e6402
jason2014 <jason2014@jason2014-PC>
2014-07-31 13:19:47 +0800

    wo

--- Rec_collection/src/com/jason/struts2/sql/Map2.java
+++ Rec_collection/src/com/jason/struts2/sql/Map2.java
@@ -2,6 +2,10 @@ package com.jason.struts2.sql;
 
 public class Map2 {
   public static void main(String[] args){
+	  
+	  
 	  System.out.println("i am yang");
+	  System.out.println("i am huang");
+	  
   }
 }
